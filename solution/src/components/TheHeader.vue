cb2f255e8ea705f0cbdc937e75da41d839957b31
Fabricio Augusto <fabricionaweb@gmail.com>
2020-10-15 23:15:34 +0100

    Create header and title component

---
+++ solution/src/components/TheHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="the-header">
+    <div class="app-container">
+      <h1>Transcriptions</h1>
+    </div>
+  </header>
+</template>
+
+<style lang="stylus" scoped>
+@import "../assets/variables"
+
+.the-header
+  height 4.3125rem
+  background-color #FFFFFF
+  box-shadow 0 1px 2px 0 rgba(0, 0, 0, 0.24),
+             0 1px 3px 0 rgba(0, 0, 0, 0.12)
+
+h1
+  color $title-color-primary
+  font-family $font-family-secondary
+  font-weight 500
+  font-size 1.125rem
+  align-self center
+</style>
